0bdd06e2a6e9ee23991a08d16b2f0284512b5888
alvarofl <alvarolima837@gmail.com>
2025-10-22 16:06:37 -0300

    criando a requisição para fazer o cadastro do medico, e será amarzenado no json, usei requestPost para enviar os dados

--- api/src/main/java/med/voll/api/ApiApplication.java
+++ api/src/main/java/med/voll/api/ApiApplication.java
@@ -1,12 +1,11 @@
 package med.voll.api;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication	
 public class ApiApplication {
 
-	public static void main(String[] args) {
+	public 		static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
