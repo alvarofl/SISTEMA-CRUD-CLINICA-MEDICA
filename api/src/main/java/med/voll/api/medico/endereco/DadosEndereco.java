70291733da69112b05003b3d59e2225ab98f4549
alvarofl <alvarolima837@gmail.com>
2025-10-23 21:46:35 -0300

    fazendo mais validações agora eu fiz dentro do controller

---
+++ api/src/main/java/med/voll/api/medico/endereco/DadosEndereco.java
@@ -0,0 +1,19 @@
+package med.voll.api.medico.endereco;
+    import  jakarta.validation.constraints.NotBlank;
+
+
+public record   DadosEndereco (
+    @NotBlank(message = "o logradouro é obrigatório")
+String logradouro,
+    @NotBlank(message = "Bairro é obrigatório")
+ String bairro,
+     @NotBlank(message = "CEP é obrigatório")
+  String cep,
+    @NotBlank(message = "Cidade é obrigatória")
+  String cidade,
+      @NotBlank(message = "UF é obrigatória")
+   String uf,
+    String complemento,
+     String numero) 
+    
+{}
